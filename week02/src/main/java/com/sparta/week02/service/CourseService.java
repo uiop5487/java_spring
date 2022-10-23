595956ed59736dd60c9088b6f4fc2cac37adf1fd
uiop5487 <uiop5487@gmail.com>
2022-10-23 21:40:51 +0900

    2022-10-23

--- week02/src/main/java/com/sparta/week02/service/CourseService.java
+++ week02/src/main/java/com/sparta/week02/service/CourseService.java
@@ -22,7 +22,7 @@ public class CourseService {
     @Transactional // SQL 쿼리가 일어나야 함을 스프링에게 알려줌
     public Long update(Long id, Course course) {
         Course course1 = courseRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다."));
+                () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다"));
         course1.update(course);
         return course1.getId();
     }
