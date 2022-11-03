1bfcfdfc07520398b93d36f28d6e5f6072735b8c
uiop5487 <uiop5487@gmail.com>
2022-11-03 11:04:45 +0900

    login test

---
+++ progix_demo/src/main/java/com/vitcon/progix_demo/ProgixDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vitcon.progix_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgixDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgixDemoApplication.class, args);
+	}
+
+}
---
+++ progix_demo/src/test/java/com/vitcon/progix_demo/ProgixDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitcon.progix_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgixDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
